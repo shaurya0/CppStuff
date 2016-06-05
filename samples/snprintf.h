8164eb48113069d7beb4c59cc7b03a5a6514f756
Shaurya Sharma <shaur141@gmail.com>
2016-06-05 20:41:55 +0200

    wip

--- samples/snprintf.h
+++ samples/snprintf.h
@@ -20,17 +20,17 @@ namespace samples
         size_t position;
     };
 
-    template<enum FMT_TYPE> struct fmt_type_to_type{};
-    template<> struct fmt_type_to_type<FMT_TYPE::INTEGER>{using type=int;};
-    template<> struct fmt_type_to_type<FMT_TYPE::STRING>{using type=std::string;};
-    template<> struct fmt_type_to_type<FMT_TYPE::CHARACTER>{using type=char;};
-    template<> struct fmt_type_to_type<FMT_TYPE::FLOAT>{using type=float;};
+    //template<enum FMT_TYPE> struct fmt_type_to_type{};
+    //template<> struct fmt_type_to_type<samples::FMT_TYPE::INTEGER>{using type=int;};
+    //template<> struct fmt_type_to_type<samples::FMT_TYPE::STRING>{using type=std::string;};
+    //template<> struct fmt_type_to_type<samples::FMT_TYPE::CHARACTER>{using type=char;};
+    //template<> struct fmt_type_to_type<samples::FMT_TYPE::FLOAT>{using type=float;};
 
-    template<typename TYPE> struct type_to_fmt_type{};
-    template<> struct type_to_fmt_type<int>{static constexpr FMT_TYPE value=FMT_TYPE::INTEGER;};
-    template<> struct type_to_fmt_type<std::string>{static constexpr FMT_TYPE value=FMT_TYPE::STRING;};
-    template<> struct type_to_fmt_type<char>{static constexpr FMT_TYPE value=FMT_TYPE::CHARACTER;};
-    template<> struct type_to_fmt_type<float>{static constexpr FMT_TYPE value=FMT_TYPE::FLOAT;};
+    //template<typename TYPE> struct type_to_fmt_type{};
+    //template<> struct type_to_fmt_type<int>{static constexpr auto value=samples::FMT_TYPE::INTEGER;};
+    //template<> struct type_to_fmt_type<std::string>{static constexpr auto value=samples::FMT_TYPE::STRING;};
+    //template<> struct type_to_fmt_type<char>{static constexpr auto value=samples::FMT_TYPE::CHARACTER;};
+    //template<> struct type_to_fmt_type<float>{static constexpr auto value=samples::FMT_TYPE::FLOAT;};
 
 
 
@@ -96,91 +96,65 @@ namespace samples
         }
     };
 
+	//template<typename ITEM>
+	//std::string to_string(Item &&item)
+	//{
+	//	return std::to_string(item);
+	//}
 
+	//template<>
+	//std::string to_string<char>(char &&item)
+	//{
+	//	return item;
+	//}
 
-    // template<typename ITEM_TYPE>
-    // void snprintf_item( char *buffer, const size_t MAX_BUFFER_LEN, const char *fmt, const std::vector<PrintfFMTMetadata> &fmt_metadata, size_t &idx, size_t &fmt_offset, ITEM_TYPE &&item)
-    // {
-    //     auto&& metadatum = fmt_metadata[idx];
-    //     const size_t position = metadatum.position;
-    //     assert( position < MAX_BUFFER_LEN );
-
-    //     // copy raw text
-    //     std::copy( fmt + fmt_offset, fmt + position + fmt_offset, buffer );
-    //     fmt_offset += position + 2;
-    //     buffer += position;
-    //     const size_t remaining_buffer = MAX_BUFFER_LEN - position;
-
-    //     const std::string value_as_string= std::to_string( item );
-    //     auto str_begin = value_as_string.cbegin();
-    //     auto str_end = value_as_string.cend();
-    //     const auto str_length = std::distance( str_begin, str_end );
-    //     assert( str_length + position < remaining_buffer );
-    //     std::copy( str_begin, str_end, buffer);
-    //     buffer += str_length;
-    //     ++idx;
-    // }
 
     template<typename ITEM_TYPE>
-    void snprintf_helper( char *buffer, const size_t MAX_BUFFER_LEN, const char *fmt, const std::vector<PrintfFMTMetadata> &fmt_metadata, size_t idx, size_t fmt_offset, ITEM_TYPE &&item)
+    void snprintf_helper( char *buffer, const size_t MAX_BUFFER_LEN, size_t &buffer_offset, const char *fmt, size_t &fmt_offset, const std::vector<PrintfFMTMetadata> &fmt_metadata, size_t &idx, ITEM_TYPE &&item)
     {
-        // snprintf_item( buffer, MAX_BUFFER_LEN, fmt, fmt_metadata, idx, fmt_offset, item );
         auto&& metadatum = fmt_metadata[idx];
         const size_t position = metadatum.position;
-        assert( position < MAX_BUFFER_LEN );
+        assert( position > fmt_offset );
+
+		const size_t num_chars = position - fmt_offset;
+		size_t buffer_end = std::min(buffer_offset + num_chars, MAX_BUFFER_LEN);
 
         // copy raw text
-        std::copy( fmt + fmt_offset, fmt + position + fmt_offset, buffer );
-        fmt_offset += position + 2;
-        buffer += position;
-        const size_t remaining_buffer = MAX_BUFFER_LEN - position;
+        std::copy( fmt + fmt_offset, fmt + position, buffer + buffer_offset );
+        buffer_offset += position - fmt_offset;
+        fmt_offset = position + 2;
+        const size_t remaining_buffer = MAX_BUFFER_LEN - buffer_offset;
 
-        const std::string value_as_string= std::to_string( item );
+        const std::string value_as_string = std::to_string( item );
         auto str_begin = value_as_string.cbegin();
         auto str_end = value_as_string.cend();
         const auto str_length = std::distance( str_begin, str_end );
         assert( str_length + position < remaining_buffer );
-        std::copy( str_begin, str_end, buffer);
-        buffer += str_length;
+        std::copy( str_begin, str_end, buffer + buffer_offset);
+        buffer_offset += str_length;
         ++idx;
+    }
+
+    template<typename ITEM_TYPE>
+    void snprintf_recurse( char *buffer, const size_t MAX_BUFFER_LEN, size_t &buffer_offset, const char *fmt, size_t &fmt_offset, const std::vector<PrintfFMTMetadata> &fmt_metadata, size_t &idx, ITEM_TYPE &&item)
+    {
+        snprintf_helper( buffer, MAX_BUFFER_LEN, buffer_offset, fmt, fmt_offset, fmt_metadata, idx, item);
     }
 
 
 
     template<typename ITEM_TYPE, typename... Args>
-    void snprintf_helper( char *buffer, const size_t MAX_BUFFER_LEN, const char *fmt, const std::vector<PrintfFMTMetadata> &fmt_metadata, size_t idx, size_t fmt_offset, ITEM_TYPE &&item, Args&&... args)
+    void snprintf_recurse( char *buffer, const size_t MAX_BUFFER_LEN, size_t &buffer_offset, const char *fmt, size_t &fmt_offset, const std::vector<PrintfFMTMetadata> &fmt_metadata, size_t &idx, ITEM_TYPE &&item, Args&&... args)
     {
-        const char *buffer_clean = buffer;
-        size_t idx_ = idx;
-        size_t fmt_offset_ = fmt_offset;
-        auto&& metadatum = fmt_metadata[idx];
-        const size_t position = metadatum.position;
-        assert( position < MAX_BUFFER_LEN );
-
-        // copy raw text
-        std::copy( fmt + fmt_offset, fmt + position + fmt_offset, buffer );
-        fmt_offset += position + 2;
-        buffer += position;
-        const size_t remaining_buffer = MAX_BUFFER_LEN - position;
-
-        const std::string value_as_string= std::to_string( item );
-        auto str_begin = value_as_string.cbegin();
-        auto str_end = value_as_string.cend();
-        const auto str_length = std::distance( str_begin, str_end );
-        assert( str_length + position < remaining_buffer );
-        std::copy( str_begin, str_end, buffer);
-        buffer += str_length;
-        ++idx;
-        snprintf_helper( buffer, MAX_BUFFER_LEN, fmt, fmt_metadata, idx, fmt_offset, args... );
-
-        // using tt = fmt_type_to_type<metadatum.type>;
+        snprintf_helper( buffer, MAX_BUFFER_LEN, buffer_offset, fmt, fmt_offset, fmt_metadata, idx, item);
+        snprintf_recurse( buffer, MAX_BUFFER_LEN, buffer_offset, fmt, fmt_offset, fmt_metadata, idx, args... );
     }
 
 
     template<typename... Args>
-    int snprintf( char *str, size_t n, const char *format, Args&&... args )
+    int snprintf( char *buffer, size_t n, const char *format, Args&&... args )
     {
-        if (nullptr == str || nullptr == format)
+        if (nullptr == buffer || nullptr == format)
         {
             return -1;
         }
@@ -190,18 +164,16 @@ namespace samples
         parse_printf_format( format, fmt_metadata );
         assert( fmt_metadata.size() == sizeof...( args ) );
 
-        // for(auto&& fmtmd : fmt_metadata)
-        // {
-        //     std::cout << (int)fmtmd.type << ", " << fmtmd.position << std::endl;
-        // }
-        snprintf_helper( str, n, format, fmt_metadata, 0, 0, args... );
+        size_t fmt_offset = 0;
+        size_t buffer_offset = 0;
+        size_t metadata_idx = 0;
+        const size_t MAX_BUFFER_LEN = n - 1; // reserve for null termination
+        snprintf_recurse( buffer, n, buffer_offset, format, fmt_offset, fmt_metadata, metadata_idx, args... );
 
+        const size_t N = strlen( format );
+        const size_t M = std::min( N, n );
 
-
-        // const size_t N = strlen( format );
-        // const size_t M = std::min( N, n );
-
-        // std::copy( format, format + M, str );
+        std::copy( format + fmt_offset, format + M, buffer + buffer_offset );
         return 0;
     }
 
